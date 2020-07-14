5cb5a4a63f0817239598d848c4e5b480e96fa91b
TJ Banghart <thomas.banghart@looker.com>
2020-07-14 14:24:27 -0700

    First client draft

---
+++ client/src/views/Landing.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+
+        <div class="position-relative">
+            <!-- shape Hero -->
+            <section class="section-shaped my-0">
+                <div class="shape shape-style-1 shape-default shape-skew">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="container shape-container d-flex">
+                    <div class="col px-0">
+                        <div class="row">
+                            <div class="col-lg">
+                                <h1 class="display-3  text-white">Welcome to Globo Gym's Workout App
+                                </h1>
+                                <p class="lead  text-white">
+                                    Get ready to burn calories and gain muscle like you wouldn't beleive
+                                </p>
+      
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- 1st Hero Variation -->
+            <section class="section-shaped my-0">
+                <div class="container shape-container d-flex justify-content-center">
+ 
+                </div>
+
+
+            </section>
+            
+        </div>
+ 
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {}
+};
+</script>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,86 @@
+<template>
+    <section class="section section-shaped section-lg my-0">
+        <div class="shape shape-style-1 bg-gradient-default">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="container pt-lg-md">
+            <div class="row justify-content-center">
+                <div class="col-lg-5">
+                    <card type="secondary" shadow
+                          header-classes="bg-white pb-5"
+                          body-classes="px-lg-5 py-lg-5"
+                          class="border-0">
+                        <template>
+                            <form role="form">
+                                <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Email"
+                                            addon-left-icon="ni ni-email-83"
+                                            :value="input.username" 
+                                            @input="val => { input.username = val }"
+                                            >
+                                </base-input>
+                                <base-input alternative
+                                            type="password"
+                                            placeholder="Password"
+                                            addon-left-icon="ni ni-lock-circle-open"
+                                            :value="input.password"
+                                            @input="val => { input.password = val }"
+                                            >
+                                </base-input>
+                                <base-checkbox
+                                    @input="val => { input.remember = val }">
+                                    Remember me
+                                </base-checkbox>
+                                <div class="text-center">
+                                    <base-button @click="log" type="primary" class="my-4">Sign In</base-button>
+                                </div>
+                            </form>
+                        </template>
+                    </card>
+                    <div class="row mt-3">
+                        <div class="col-6">
+                            <a href="#" class="text-light">
+                                <small>Forgot password?</small>
+                            </a>
+                        </div>
+                        <div class="col-6 text-right">
+                            <router-link to="/register" class="text-light">
+                                <small>Create new account</small>
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            input: {
+                username: "",
+                password: "",
+                remember: false
+            }
+        }
+    },
+    methods: {
+        log: function(val) {
+            console.log(this.input.username)
+            console.log(this.input.password)
+            console.log(this.input.remember)
+        }
+    }
+}
+</script>
+<style>
+</style>
