59b614f29c33beac0c0d065a070630863c453c0d
dyllanv <55823152+dyllanv@users.noreply.github.com>
2020-08-06 22:31:53 -0700

    created function to make all queries

--- flask_app/main.py
+++ flask_app/main.py
@@ -8,18 +8,22 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 CORS(app)
 
 
-def get_workout_list(user_id, workout_list, table_id):
+def make_query(query):
     cnx = mariadb.connect(user='vagrant', password='password', database='cobras')
+    cursor = cnx.cursor(dictionary=True)
+    cursor.execute(query)
+    data = cursor.fetchall()
+    cnx.close()
+    return data
+
+def get_workout_list(user_id, workout_list, table_id):
     query = (
         "SELECT w.name FROM workouts w " 
         "JOIN " + workout_list + " x ON w.workout_id = x.workout_id "
         "JOIN users u ON x.user_id = u.user_id "
         "WHERE u.user_id = " + str(user_id) + " ORDER BY " + table_id + " DESC;"
     )
-    cursor = cnx.cursor(dictionary=True)
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cnx.close()
+    data = make_query(query)
     return data
 
 @app.route('/completed/<user_id>')
@@ -39,14 +43,10 @@ def get_todo(user_id):
 
 @app.route('/user/<user_id>')
 def get_user(user_id):
-    cnx = mariadb.connect(user='vagrant', password='password', database='cobras')
     query = (
         "SELECT username, birthdate, gender FROM users WHERE user_id = " + str(user_id) + ";"
     )
-    cursor = cnx.cursor(dictionary=True)
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cnx.close()
+    data = make_query(query)
     for i in data:
         i['birthdate'] = i['birthdate'].strftime('%m/%d/%Y')
 
@@ -54,56 +54,38 @@ def get_user(user_id):
 
 @app.route('/workout/<workout_id>')
 def get_workout(workout_id):
-    cnx = mariadb.connect(user='vagrant', password='password', database='cobras')
     query = (
         "SELECT * FROM workouts WHERE workout_id = " + str(workout_id) + ";"
     )
-    cursor = cnx.cursor(dictionary=True)
-    cursor.execute(query)
-    data = cursor.fetchall()
-    print(data)
-    cnx.close()
+    data = make_query(query)
     data[0]['rating'] = get_workout_rating(workout_id)
     return json.dumps(data)
 
 @app.route('/allworkouts')
 def get_all_workouts():
-    cnx = mariadb.connect(user='vagrant', password='password', database='cobras')
     query = (
         "SELECT * FROM workouts ORDER BY workout_id DESC;"
     )
-    cursor = cnx.cursor(dictionary=True)
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cnx.close()
+    data = make_query(query)
     for workout in data:
         workout['rating'] = get_workout_rating(workout['workout_id'])
     return json.dumps(data)
 
 def get_workout_rating(workout_id):
-    cnx = mariadb.connect(user='vagrant', password='password', database='cobras')
     query = (
         "SELECT AVG(wr.rating) AS rating FROM workout_ratings wr "
         "JOIN workouts w ON wr.workout_id = w.workout_id "
         "WHERE wr.workout_id = " + str(workout_id) +";"
     )
-    cursor = cnx.cursor(dictionary=True)
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cnx.close()
+    data = make_query(query)
     return str(data[0]['rating'])
 
 @app.route('/forum')
 def get_forum():
-    cnx = mariadb.connect(user='vagrant', password='password', database='cobras')
     query = (
         "SELECT thread_id, name, datetime FROM threads ORDER BY DATETIME DESC;"
     )
-
-    cursor = cnx.cursor(dictionary=True)
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cnx.close()
+    data = make_query(query)
     for i in data:
         i['datetime'] = i['datetime'].strftime('%m/%d/%Y')
         i['rating'] = get_thread_rating(i['thread_id'])
@@ -112,16 +94,12 @@ def get_forum():
 
 @app.route('/thread/<thread_id>')
 def get_thread(thread_id):
-    cnx = mariadb.connect(user='vagrant', password='password', database='cobras')
     query = (
         "SELECT u.username AS 'username', t.name as 'thread_name', t.datetime, t.content FROM threads t "
         "JOIN users u ON t.user_id = u.user_id "
         "WHERE t.thread_id = " + thread_id + ";"
     )
-    cursor = cnx.cursor(dictionary=True)
-    cursor.execute(query)
-    thread_info = cursor.fetchall()
-    cnx.close()
+    thread_info = make_query(query)
     for i in thread_info:
         i['datetime'] = i['datetime'].strftime('%m/%d/%Y')
 
@@ -134,57 +112,39 @@ def get_thread(thread_id):
     return json.dumps(data)
 
 def get_thread_comments(thread_id):
-    cnx = mariadb.connect(user='vagrant', password='password', database='cobras')
     query = (
         "SELECT u.username AS 'username', c.datetime, c.content FROM comments c "
         "JOIN users u ON c.user_id = u.user_id "
         "JOIN threads t ON c.thread_id = t.thread_id "
         "WHERE t.thread_id = " + thread_id + ";"
     )
-    cursor = cnx.cursor(dictionary=True)
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cnx.close()
+    data = make_query(query)
     for i in data:
         i['datetime'] = i['datetime'].strftime('%m/%d/%Y')
 
     return data
 
 def get_pictures(thread_id):
-    cnx = mariadb.connect(user='vagrant', password='password', database='cobras')
     query = (
         "SELECT link FROM pictures WHERE thread_id = " + thread_id + ";"
     )
-    cursor = cnx.cursor(dictionary=True)
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cnx.close()
-
+    data = make_query(query)
     return data
 
 def get_videos(thread_id):
-    cnx = mariadb.connect(user='vagrant', password='password', database='cobras')
     query = (
         "SELECT link FROM videos WHERE thread_id = " + thread_id + ";"
     )
-    cursor = cnx.cursor(dictionary=True)
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cnx.close()
-
+    data = make_query(query)
     return data
 
 def get_thread_rating(thread_id):
-    cnx = mariadb.connect(user='vagrant', password='password', database='cobras')
     query = (
         "SELECT AVG(tr.rating) AS rating FROM thread_ratings tr "
         "JOIN threads t ON tr.thread_id = t.thread_id "
         "WHERE tr.thread_id = " + str(thread_id) + ";"
     )
-    cursor = cnx.cursor(dictionary=True)
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cnx.close()
+    data = make_query(query)
     return str(data[0]['rating'])
 
 if __name__ == '__main__':
