d19b56928fdf3ba9019ac926c83f6be2c3d2b63d
edwarsco-oregonstate-edu <31459103+edwarsco-oregonstate-edu@users.noreply.github.com>
2020-08-09 21:27:07 -0500

    Removed unnecessary code

--- router.js
+++ router.js
@@ -4,12 +4,6 @@ import Home from './views/Home.vue'
 import Category from './views/Category.vue'
 import Topic from './views/Topic.vue'
 import CreateTopic from './views/CreateTopic.vue'
-import EditTopic from './views/EditTopic.vue'
-import CreateCategory from './views/CreateCategory.vue'
-import EditCategory from './views/EditCategory.vue'
-import Login from './views/Login.vue'
-import Register from './views/Register.vue'
-import AccountSettings from './views/AccountSettings.vue'
 import NotFound from './views/NotFound.vue'
 
 Vue.use(Router)
@@ -21,48 +15,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login,
-      meta: {
-        guest: true
-      }
-    },
-    {
-      path: '/register',
-      name: 'Register',
-      component: Register,
-      meta: {
-        guest: true
-      }
-    },
-    {
-      path: '/account-settings',
-      name: 'AccountSettings',
-      component: AccountSettings,
-      meta: {
-        auth: true
-      }
-    },
-    {
-      path: '/new-category',
-      name: 'CreateCategory',
-      component: CreateCategory,
-      meta: {
-        auth: true,
-        permissions: ['categories:write']
-      }
-    },
-    {
-      path: '/c/:categorySlug/edit',
-      name: 'EditCategory',
-      component: EditCategory,
-      meta: {
-        auth: true,
-        permissions: ['categories:write']
-      }
     },
     {
       path: '/c/:categorySlug',
@@ -78,12 +30,6 @@ export default new Router({
       path: '/c/:categorySlug/new-topic',
       name: 'CreateTopic',
       component: CreateTopic,
-      meta: { auth: true }
-    },
-    {
-      path: '/t/:topicId/edit',
-      name: 'EditTopic',
-      component: EditTopic,
       meta: { auth: true }
     },
     {
